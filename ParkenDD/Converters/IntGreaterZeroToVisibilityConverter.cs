b35c6bff540511edcae08f529c363f40f9b21867
Sebastian Müller <me@sibbl.net>
2015-09-20 17:57:37 +0200

    Fix visualization for parking lots with unknown total lot count

---
+++ ParkenDD/Converters/IntGreaterZeroToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace ParkenDD.Converters
+{
+    public class IntGreaterZeroToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(value is int))
+            {
+                return false;
+            }
+            var num = (int) value;
+            return num > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
