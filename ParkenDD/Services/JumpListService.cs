3d604f68eb5e4a6595b13f1af44d2ef8110079c5
Sebastian Müller <me@sibbl.net>
2015-11-08 10:18:30 +0100

    Refactor jump list support a bit

--- ParkenDD/Services/JumpListService.cs
+++ ParkenDD/Services/JumpListService.cs
@@ -28,22 +28,18 @@ namespace ParkenDD.Services
             }
             if (JumpList.IsSupported())
             {
-                var jl = await JumpList.LoadCurrentAsync();
-                jl.SystemGroupKind = JumpListSystemGroupKind.None;
-
-                while (jl.Items.Count > 0)
-                {
-                    jl.Items.RemoveAt(0);
-                }
+                var jumpList = await JumpList.LoadCurrentAsync();
+                jumpList.SystemGroupKind = JumpListSystemGroupKind.None;
+                jumpList.Items.Clear();
 
                 foreach (var city in cities)
                 {
                     var item = JumpListItem.CreateWithArguments(string.Format(ArgumentFormat, city.Id), city.Name);
                     item.GroupName = _resources.JumpListCitiesHeader;
                     item.Logo = new Uri("ms-appx:///Assets/ParkingIcon.png");
-                    jl.Items.Add(item);
+                    jumpList.Items.Add(item);
                 }
-                await jl.SaveAsync();
+                await jumpList.SaveAsync();
             }
         }
     }
